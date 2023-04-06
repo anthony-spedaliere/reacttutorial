8642a497873730f57f942de6448f0ef9d28b7db0
Anthony Spedaliere <avspedaliere@gmail.com>
2023-04-06 16:03:28 -0700

    Completed first hour of tutorial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// React Component Basic Rules
+// start with capital letter
+// must return JSX (html)
+// always close tag
+
+// Javascript entry point - connects the index.js root to the public index.html entry point root
+function Greeting() {
+    return <h2>React Tutorial</h2>;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Greeting />);
