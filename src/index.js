7aab6bb4a04e258e85a7393629896aa6dbb2d242
Anthony Spedaliere <avspedaliere@gmail.com>
2023-04-06 21:52:01 -0700

    Finished section on components

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 
 // React Component Basic Rules
 // function name starts with capital letter
-// must return JSX (html)
+// must return JSX (html) - only one parent element (cannot return adjacent elements). React.Fragment can be used or <> </>
+
 // always close tag
 
 // Javascript entry point - connects the index.js root to the public index.html entry point root
-function Greeting() {
-    return <h2>React Tutorial</h2>;
+function App() {
+    return (<h2>React Tutorial</h2>);
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Greeting />);
+root.render(<App />);
