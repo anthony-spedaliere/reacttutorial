6703db750abee71b0c9c899829d817be1796eef6
Anthony Spedaliere <avspedaliere@gmail.com>
2023-04-07 19:40:31 -0700

    Worked through JSX for CSS and Javascript sections of tutorial

--- src/index.js
+++ src/index.js
@@ -6,8 +6,34 @@ import './index.css';
 // React Component Basic Rules
 // function name starts with capital letter
 // must return JSX (html) - only one parent element (cannot return adjacent elements). React.Fragment can be used or <> </>
-
 // always close tag
+
+const books = [
+    {
+        id : 0,
+        imageURL: 'https://images-na.ssl-images-amazon.com/images/I/810bsxh1MmL._AC_UL600_SR600,400_.jpg',
+        title : 'How to Catch the Easter Bunny',
+        Author: 'Adam Wallace (Author) and Andy Elkerton (Illustrator)'
+    },
+    {
+        id : 1,
+        imageURL: 'https://images-na.ssl-images-amazon.com/images/I/81fyoFoaxlL._AC_UL600_SR600,400_.jpg',
+        title : 'Dog Man: Twenty Thousand Fleas Under the Sea: A Graphic Novel (Dog Man #11): From the Creator of Captain Underpants',
+        author: 'Dave Pilkey'
+    },
+    {
+        id : 2,
+        imageURL: 'https://images-na.ssl-images-amazon.com/images/I/913UqrSJf6L._AC_UL600_SR600,400_.jpg',
+        title : 'The Return of the Gods',
+        author: 'Jonathan Cahn'
+    },
+    {
+        id : 3,
+        imageURL: 'https://images-na.ssl-images-amazon.com/images/I/61RnYtO-QUL._AC_UL600_SR600,400_.jpg',
+        title : 'Got Your Number: The Greatest Sports Legends and the Numbers They Own',
+        author: 'Mike Greenberg'
+    },
+]
 
 // Javascript entry point - connects the index.js root to the public index.html entry point root
 function BookList() {
@@ -22,18 +48,18 @@ function BookList() {
 }
 
 const Book = () => {
+    const imageURL = 'https://images-na.ssl-images-amazon.com/images/I/810bsxh1MmL._AC_UL600_SR600,400_.jpg'
+    const bookTitle = 'How to Catch the Easter Bunny'
+    const bookAuthor = 'Adam Wallace (Author) and Andy Elkerton (Illustrator)'
+
     return (
         <article className='book'>
-            <Image />
-            <Title />
-            <Author />
+            <img src= {imageURL}/>
+            <h2>Title: {bookTitle}</h2>
+            <h4>By: {bookAuthor}</h4>
         </article>
     );
 }
-
-const Image = () => <img src='https://images-na.ssl-images-amazon.com/images/I/810bsxh1MmL._AC_UL600_SR600,400_.jpg' />;
-const Title = () => <h2>Title: How to Catch the Easter Bunny</h2>;
-const Author = () => <h2>By: Adam Wallace (Author) and Andy Elkerton (Illustrator)</h2>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
