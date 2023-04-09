6acc31a773301cd7dddb086dfa65c331e72a032b
Anthony Spedaliere <avspedaliere@gmail.com>
2023-04-08 18:13:02 -0700

    Refactored the props syntax

--- src/index.js
+++ src/index.js
@@ -48,12 +48,13 @@ function BookList() {
 }
 
 const Book = (props) => {
+    const { imageURL, title, author } = props;
     
     return (
         <article className='book'>
-            <img src= {props.imageURL} alt={props.title}/>
-            <h2>Title: {props.title}</h2>
-            <h4>By: {props.author}</h4>
+            <img src= {imageURL} alt={title}/>
+            <h2>Title: {title}</h2>
+            <h4>By: {author}</h4>
         </article>
     );
 }
